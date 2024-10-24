c11ab81e762e4e83cf733c77d057888b852ebdef
Divine Gem <diyvaratan8923@outlook.com>
2024-10-24 19:10:40 +0530

    Some random iteration questions.

---
+++ Day35Week8/Question1.cpp
@@ -0,0 +1,22 @@
+/*There is a road, which can be represented as a number line. You are located in the point 0 of the number line, and you want to travel from the point 0 to the point 𝑥, and back to the point 0.
+
+You travel by car, which spends 1 liter of gasoline per 1 unit of distance travelled. When you start at the point 0, your car is fully fueled (its gas tank contains the maximum possible amount of fuel).
+
+There are 𝑛 gas stations, located in points 𝑎1,𝑎2,…,𝑎𝑛. When you arrive at a gas station, you fully refuel your car. Note that you can refuel only at gas stations, and there are no gas stations in points 0 and 𝑥.
+
+You have to calculate the minimum possible volume of the gas tank in your car (in liters) that will allow you to travel from the point 0 to the point 𝑥 and back to the point 0.*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++){
+        int n, x;
+        cin >> n >> x;
+        int *arr = new int[n];
+        for (int j = 0; j < n; j++)
+            cin >> arr[j];
+        
+    }
+}
